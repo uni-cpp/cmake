fe3e6a096912f10337091fd3752891c6c69d8dde
Sergey Polyakov <white.irbys@gmail.com>
2021-02-05 20:06:34 +0200

    Improve coverage generation

--- coverage.cmake
+++ coverage.cmake
@@ -1,5 +1,9 @@
 find_program( GCOV gcov )
 
+##########################################
+# Basic checks
+##########################################
+
 if( CODE_COVERAGE AND GCOV)
     message( STATUS "Code coverage requested and will be used" )
     set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage" )
@@ -15,9 +19,11 @@ else( )
     return( )
 endif( )
 
-#####################
+##########################################
 # Coverage generators
-#####################
+##########################################
+
+set( COVERAGE_OUTPUT_DIR_NAME "out" )
 
 if( USE_LCOV)
     find_program( LCOV lcov )
@@ -31,7 +37,8 @@ if( USE_LCOV)
     endif( )
 
     set( LCOV_OPTIONS "--capture --directory . --output-file coverage.info" )
-    set( GENHTML_OPTIONS "coverage.info --output-directory out --function-coverage --legend --demangle-cpp" )
+    # --demangle-cpp required c++filt
+    set( GENHTML_OPTIONS "coverage.info --output-directory ${COVERAGE_OUTPUT_DIR_NAME} --function-coverage --legend --demangle-cpp" )
     if( BRANCH_COVERAGE )
         set( GENHTML_OPTIONS "${GENHTML_OPTIONS} --branch-coverage" )
     endif( )
@@ -45,31 +52,44 @@ if( USE_GCOVR )
         message( FATAL_ERROR "GCOVR requested but not found" )
     endif( )
 
-    # CMAKE_CURRENT_SOURCE_DIR probably will be more reliable
-    set( GCOVR_OPTIONS "--root ${CMAKE_CURRENT_SOURCE_DIR} --html --html-details --output index.html --object-directory ${CMAKE_BINARY_DIR}" )
+    set( GCOVR_OPTIONS "--html --html-details --output ${COVERAGE_OUTPUT_DIR_NAME}/index.html" )
 
     message( STATUS "GCOVR code coverage generator is enabled" )
 endif( )
 
-function( generate_coverage PROJECT_NAME )
+##########################################
+# Function for CMakeLists.txt
+##########################################
+
+set( RUN_POSTFIX "run" )
+set( COVERAGE_POSTFIX "coverage" )
+
+# TODO DEPENDED_TESTS should be ARGV
+function( generate_coverage PROJECT_NAME DEPENDED_TESTS)
+    set( ALL_DEPENDED_TESTS ${DEPENDED_TESTS}_${RUN_POSTFIX} )
+    set( COVERAGE_TARGET_NAME ${PROJECT_NAME}_${COVERAGE_POSTFIX} )
+
     if( USE_LCOV )
-        set( LCOV_FILTER_OPTIONS "--include \"*${PROJECT_NAME}*\"" )
-        # TODO exclude all tests and apps files
+        # set( LCOV_FILTER_OPTIONS "--include \"*${PROJECT_NAME}*\"" )
         set( LCOV_FULL_COMMAND "${LCOV} ${LCOV_OPTIONS} ${LCOV_FILTER_OPTIONS}" )
         separate_arguments( LCOV_FULL_COMMAND )
         set( GENHTML_FULL_COMMAND "${GENHTML} ${GENHTML_OPTIONS}" )
         separate_arguments( GENHTML_FULL_COMMAND )
-        add_custom_target( ${PROJECT_NAME}_coverage ALL
+        add_custom_target( ${COVERAGE_TARGET_NAME} ALL
                            COMMAND ${LCOV_FULL_COMMAND}
                            COMMAND ${GENHTML_FULL_COMMAND}
-                           DEPENDS ${PROJECT_NAME}_run )
+                           COMMENT "Generating coverage using LCOV for ${PROJECT_NAME}. Depended tests: ${DEPENDED_TESTS}"
+                           DEPENDS ${ALL_DEPENDED_TESTS} )
     elseif( USE_GCOVR )
         # TODO add filters
-        set( GCOVR_FULL_COMMAND "${GCOVR} ${GCOVR_OPTIONS}" )
+        set( GCOVR_ADDITIONAL_OPTIONS "--root ${CMAKE_CURRENT_SOURCE_DIR} --object-directory ${CMAKE_CURRENT_BINARY_DIR}" )
+        set( GCOVR_FULL_COMMAND "${GCOVR} ${GCOVR_OPTIONS}" ${GCOVR_ADDITIONAL_OPTIONS} )
         separate_arguments( GCOVR_FULL_COMMAND )
-        add_custom_target( ${PROJECT_NAME}_coverage ALL
+        add_custom_target( ${COVERAGE_TARGET_NAME} ALL
+                           COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_OUTPUT_DIR_NAME}
                            COMMAND ${GCOVR_FULL_COMMAND}
-                           DEPENDS ${PROJECT_NAME}_run )
+                           COMMENT "Generating coverage using GCOVR for ${PROJECT_NAME}. Depended tests: ${DEPENDED_TESTS}"
+                           DEPENDS ${ALL_DEPENDED_TESTS} )
     else( )
         message( FATAL_ERROR "Coverage generator LCOV or GCOVR should be acivated" )
     endif( )
